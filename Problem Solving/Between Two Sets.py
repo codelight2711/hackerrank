1f9363155e7e75dbf505ce8fb69fba20488aa3be
utkarsh srivastava <60793964+major-sri@users.noreply.github.com>
2020-04-19 16:19:16 +0530

    Update Between Two Sets.py

--- Problem Solving/Between Two Sets.py
+++ Problem Solving/Between Two Sets.py
@@ -1,22 +1,22 @@
 def gcd(a,b):
-    """Return greatest common divisor using Euclid's Algorithm."""
+   
     while b:      
         a, b = b, a % b
     return a
 
 def lcm(a, b):
-    """Return lowest common multiple."""
+    
     return a * b / gcd(a, b)
 
 def GCD(terms):
-    "Return gcd of a list of numbers."
+   
     result = terms[0]
     for i in range(1, len(terms)):
         result = gcd(result, terms[i])
     return result
 
 def LCM(terms):
-    "Return lcm of a list of numbers."
+   
     result = 1
     for t in terms:
         result = lcm(result, t)
