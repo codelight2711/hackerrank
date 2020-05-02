afeca16fc21443294721b58cab1993c8e0bb5d49
utkarsh srivastava <60793964+major-sri@users.noreply.github.com>
2020-05-02 20:57:00 +0530

    Create Python If-Else.py

---
+++ Python/Python If-Else.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+check = {True: "Not Weird", False: "Weird"}
+
+print(check[
+        n%2==0 and (
+            n in range(2,6) or 
+            n > 20)
+    ])
